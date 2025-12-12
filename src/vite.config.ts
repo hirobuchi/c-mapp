9e4d3be2a0d02f738d6c69323d08e7102ded229a
hiromsbc <hiro159m@gmail.com>
2025-12-12 22:43:42 +0900

    react-scriptからviteへの移行

---
+++ src/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  server: {
+    open: true,
+    port: 3000,
+  },
+  build: {
+    outDir: 'build',
+  },
+  plugins: [react()],
+});
